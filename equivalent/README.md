b09f7a3122971d88085e60af480c635d262758a0
tkhwang <tkhwang@gmail.com>
2017-01-10 23:17:17 +0900

    [+] 2017.01.10 Udemy  python-video-workbook

--- equivalent/README.md
+++ equivalent/README.md
@@ -5,3 +5,6 @@
 - 2016.12.23 : 3DSCTF : Pwn : Stage3 - Please, no
 
 
+## Udemy : python-video-workbook
+
+- 2017.01.10 : problem solving
---
+++ udemy/the_python_video_workbook_solve_100_exercies/22.py
@@ -0,0 +1,6 @@
+from pprint import pprint
+
+d = dict(a = list(range(1, 11)), b = list(range(11, 21)), c = list(range(21, 31)))
+
+print(d)
+pprint(d)
