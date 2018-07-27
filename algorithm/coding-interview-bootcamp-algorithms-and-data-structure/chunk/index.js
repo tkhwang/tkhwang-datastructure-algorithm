5cf25730346a7fdc9744ecdf1cd2f501fcdaa4cd
tkhwang <tkhwang@gmail.com>
2018-07-27 20:11:25 +0900

    [+] 3/100 #100DaysOfCode Chunk #2 in javascript.

--- algorithm/coding-interview-bootcamp-algorithms-and-data-structure/chunk/index.js
+++ algorithm/coding-interview-bootcamp-algorithms-and-data-structure/chunk/index.js
@@ -8,6 +8,7 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+/*
 function chunk(array, size) {
   const chunked = [];
 
@@ -19,6 +20,19 @@ function chunk(array, size) {
     } else {
       last.push(element);
     }
+  }
+
+  return chunked;
+}
+*/
+
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
   }
 
   return chunked;
