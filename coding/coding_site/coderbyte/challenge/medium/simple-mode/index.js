6c2a28069597bf4e064b23667d6e9bd1fedcca43
tkhwang <tkhwang@gmail.com>
2018-09-21 14:54:18 +0900

    [+] coderbyte javascript problem solving added.

---
+++ coding/coding_site/coderbyte/challenge/medium/simple-mode/index.js
@@ -0,0 +1,20 @@
+function SimpleMode(arr) {
+
+  let output = {};
+  let max = 0
+
+  arr.forEach(function (el) {
+    if (output.hasOwnProperty(el)) {
+      output[el]++;
+    } else {
+      output[el] = 1;
+    }
+  });
+
+  let key = Object.keys(output);
+  key.sort((a, b) => output[b] - output[a]);
+
+  return output[key[0]] === 1 ? -1 : Number(key[0]);
+}
+
+module.exports = SimpleMode;
