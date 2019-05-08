7b0a36f6f2cf717b4cf718c0ab3de4a3507a596c
tkhwang <tkhwang@gmail.com>
2019-05-08 19:20:50 +0900

    leetcode.com /easy @ May.8.2019

--- coding/_site/leetcode.com/easy/backspace-string-compare/index.js
+++ coding/_site/leetcode.com/easy/backspace-string-compare/index.js
@@ -3,20 +3,40 @@
  * @param {string} T
  * @return {boolean}
  */
+
+// #1
+/*
 var backspaceCompare = function(S, T) {
   const newS = [];
   for (let i = 0; i < S.length; i += 1) {
-    if (S[i] === "#") {
+    if (S[i] === '#') {
       newS.pop();
     } else newS.push(S[i]);
   }
 
   const newT = [];
   for (let i = 0; i < T.length; i += 1) {
-    if (T[i] === "#") {
+    if (T[i] === '#') {
       newT.pop();
     } else newT.push(T[i]);
   }
 
-  return newS.join("") === newT.join("");
+  console.log(newS, newT);
+  return newS.join('') === newT.join('');
+};
+*/
+
+// #2
+var backspaceCompare = function(S, T) {
+  const handleSharp = str => {
+    const result = [];
+    for (let i = 0; i < str.length; i += 1) {
+      if (str[i] === "#") result.pop();
+      else result.push(str[i]);
+    }
+
+    return result.join("");
+  };
+
+  return handleSharp(S) === handleSharp(T);
 };
