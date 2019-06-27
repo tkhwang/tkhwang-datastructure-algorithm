22cb180b285af0537ba02010ca9ad87bfdc75611
tkhwang <tkhwang@gmail.com>
2019-06-27 11:30:38 +0900

    leetcode / easy / 215.kth-largest-element-in-an-array

---
+++ coding/algorithm/leetcode.com/easy/215.kth-largest-element-in-an-array/215.kth-largest-element-in-an-array.js
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=215 lang=javascript
+ *
+ * [215] Kth Largest Element in an Array
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+  nums.sort((a, b) => b - a);
+  return nums[k - 1];
+};
