9f7d66dc0fdf73434a04443eb9126bcc2307187d
Taekeun Hwang <tkhwang@gmail.com>
2020-09-19 15:49:40 +0900

    Rearrange folders

--- leetcode/_1.easy/palindrome-number/index.js
+++
@@ -1,6 +0,0 @@
-var isPalindrome = function (x) {
-  return ('' + x) === ('' + x).split('').reverse().join('')
-
-};
-
-module.exports = isPalindrome;
--- leetcode/_1.easy/palindrome-number/index.test.js
+++
@@ -1,7 +0,0 @@
-const isPalindrome = require("./index");
-
-describe("isPalindrome", () => {
-  it("should fail auto generated test", () => {
-    expect(isPalindrome(121)).toBe(true);
-  });
-});
